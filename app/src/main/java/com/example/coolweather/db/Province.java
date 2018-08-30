3a016a97396c670127cd43c6d7c8d4b5ca87c379
liangei8402 <liangfei8402@163.com>
2018-08-30 21:55:19 +0800

    完成便利省市县三级列表功能。

--- app/src/main/java/com/example/coolweather/db/Province.java
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -19,7 +19,7 @@ public class Province extends DataSupport {
         return id;
     }
 
-    public void setId(){
+    public void setId(int id){
         this.id = id;
     }
 
---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,20 @@
+package com.example.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * 包名com.example.coolweather.util
+ * Created by liangfei on 2018/8/26.
+ * 该类主要实现
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String adress, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(adress)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+}
